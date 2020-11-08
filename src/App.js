3c4ca3c05cdb6858993b6253938af6157c870f8e
Brice d'Arménia <brice-d-armenia_student2020@wilder.school>
2020-11-08 12:48:12 +0100

    Contact OK

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React from "react";
 import { Contact } from "./Components/Contact";
+const dee_dee = require("./images/dee-dee.png");
+const joey = require("./images/joey.png");
+const marky = require("./images/marky.jpeg");
 
 function App() {
   return (
     <div className="App">
-      <Contact />
+      <Contact name="Joey" avatar={joey} online />
+      <Contact name="Dee-Dee" avatar={dee_dee} />
+      <Contact name="Marky" avatar={marky} online />
     </div>
   );
 }
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,20 +1,24 @@
-import './Contact.css';
+import React from "react";
+import Proptypes from "prop-types";
+import "./Contact.css";
 
-const name = "Goldorak"
-const avatar = 'url'
-const online = true
-
-const Contact = () => (
-    <div className="Contact">
-        <img className="avatar" src={avatar} alt="avatar" />
-        <div>
-            <h4 className="name">{name}</h4>
-            <p className="status" >
-                <span className={ online ? 'status-online' : 'status-offline'}/>
-                { online ? 'Online' : 'Offline' }
-            </p>
-        </div>
+const Contact = ({ name, avatar, online }) => (
+  <div className="Contact">
+    <img className="avatar" src={avatar} alt="avatar" />
+    <div>
+      <h4 className="name">{name}</h4>
+      <div className="status">
+        <span className={online ? "status-online" : "status-offline"} />
+        <p className="status-text">{online ? "Online" : "Offline"}</p>
+      </div>
     </div>
-)
+  </div>
+);
 
-export { Contact }
+Contact.propTypes = {
+  name: Proptypes.string.isRequired,
+  avatar: Proptypes.string.isRequired,
+  online: Proptypes.bool
+};
+
+export { Contact };
